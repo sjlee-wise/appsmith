31a5ecf3296ebf2946b103ef60d29fda5d9f8bda
sjlee <sjlee@wise.co.kr>
2025-11-25 08:58:08 +0000

    파일 업로드

--- pages/Page2/jsobjects/JSObject1/JSObject1.js
+++ pages/Page2/jsobjects/JSObject1/JSObject1.js
@@ -1,8 +1,19 @@
 export default {
-	myVar1: [],
-	myVar2: {},
+	async captureAndUpload() {
+		const stream = await Camera1.start(); // 카메라 스트림 가져오기
+		const imageBitmap = await createImageBitmap(stream);
+		const canvas = document.createElement("canvas");
+		canvas.width = imageBitmap.width;
+		canvas.height = imageBitmap.height;
+		const ctx = canvas.getContext("2d");
+		ctx.drawImage(imageBitmap, 0, 0);
+		const dataUrl = canvas.toDataURL("image/jpeg");
 
-	// async onClick () {
-	// await storeValue("capturedImg", Camera1.imageBlobURL); 
-	// }
+		// S3 Query 호출
+		UploadToS3.run({
+			Content: dataUrl.split(",")[1],
+			ContentType: "image/jpeg",
+			Key: `photos/${Date.now()}.jpg`
+		});
+	}
 }
